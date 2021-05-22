67f2c23d2279b06a46a1de6e7669e3f7b5996fca
guacorp <giveusanythingcorp@gmail.com>
2021-05-22 22:48:00 +0530

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from "./components/Navbar.js"
+import Header from "./components/Header.js"
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Header />
+    </>  
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+const Header = () => {
+    return (
+    <div className = "header-wrapper">
+        <div className = "main-info">
+            <h1>Use AI on Image and Video</h1>
+        </div>
+    </div>
+    )
+}
+
+export default Header
